ce2794951a6811b0c3ba357b9a89a688d9ed2f3c
Nick Chin <nick.m.chin12@gmail.com>
2026-03-12 23:07:54 -0400

    Initial stages of a proper log-in feature

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/login', methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        user = request.form['nm']
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+
 @app.route('/<name>') # route to the logged-in user homepage
 def user_home(name):
     return render_template("user_home.html", content = name)
